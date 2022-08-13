d4ecc4969d773211af8f3b677d001e9f34915237
Jacob <jacobulysseslockwood@gmail.com>
2022-08-13 14:31:57 -0400

    Move to Astro

--- src/restdb.js
+++
@@ -1,9 +0,0 @@
-const url = "https://gordonlockwoodevents-cf27.restdb.io/rest/events";
-const key = "61478cf5dfa7346e2f968e99";
-export const eventsPromise = fetch(url, {
-  headers: {
-    "content-type": "application/json",
-    "x-apikey": key,
-    "cache-control": "no-cache",
-  },
-}).then((res) => res.json());
